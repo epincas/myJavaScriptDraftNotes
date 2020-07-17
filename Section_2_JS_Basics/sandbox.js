a951c61b0cb75b729343903dcdd923faafe419cb
Ted Pincas <epincas@gmail.com>
2020-07-17 17:58:52 +0200

    no comments

--- Section_2_JS_Basics/sandbox.js
+++ Section_2_JS_Basics/sandbox.js
@@ -1,1 +1,19 @@
-alert('Hello, world')
+//alert('Hello, world');
+
+console.log(1);
+console.log(2);
+
+let age = 25;
+let year = 2019;
+console.log(age, year);
+
+age = 30;
+console.log(age);
+
+/* const points = 100;
+ points = 50;
+console.log(points);
+ */
+
+var score = 75; // old way to declare a variable;
+console.log(score);
